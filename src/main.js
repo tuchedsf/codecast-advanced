25f8c648cd7eb0f55a4617dbc2f8a833ed061eb2
Diego Ferreira <diego.diegoferreira@gmail.com>
2017-04-05 22:48:04 -0300

    configurando projeto para usar o vuex- video configurando vuex part 1'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App';
 
+import store from './vuex/store';
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
+  store,
   el: '#app',
   render: h => h(App),
 });
